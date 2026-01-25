5ea90f140d4873ba551814ff9f8f074ee18a29eb
V <121571470+MrV1777@users.noreply.github.com>
2026-01-25 20:55:10 +0700

    005

--- project_shop/routes/web.php
+++ project_shop/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+use App\Http\Controllers\Auth\AuthController;
+
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::post('post-login', [AuthController::class, 'postLogin'])->name('login.post');
