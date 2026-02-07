0dbb98b2c169d523333a714fc992f6b8301c3a04
V <121571470+MrV1777@users.noreply.github.com>
2026-02-08 00:43:42 +0700

    0012

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\StudentController;
 
-
-Route::get('/', [StudentController::class, 'index'])->name('students.index');
-Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
-Route::get('/students/{id}', [StudentController::class, 'show'])->name('students.show');
-Route::get('/students/{id}/edit', [StudentController::class, 'edit'])->name('students.edit');
-Route::post('/students/store', [StudentController::class, 'store'])->name('students.store');
-Route::put('/students/{id}', [StudentController::class, 'update'])->name('students.update');
-Route::delete('/students/{id}', [StudentController::class, 'destroy'])->name('students.destroy');
+Route::get('/', function () {
+    return view('index');
+});
