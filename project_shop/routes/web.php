7d43df39b25aee41e880c5d317cd8287d8fb4a39
V <121571470+MrV1777@users.noreply.github.com>
2026-02-07 23:15:47 +0700

    001

--- project_shop/routes/web.php
+++ project_shop/routes/web.php
@@ -4,8 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 
+// Home Route
+Route::get('/', function () {
+    return view('home.index');
+})->name('home');
+
 // Authentication Routes
-Route::get('/', [AuthController::class, 'index'])->name('login');
+Route::get('/login', [AuthController::class, 'index'])->name('index');
 Route::post('/login', [AuthController::class, 'postLogin'])->name('login.post');
 Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [AuthController::class, 'register'])->name('register.post');
