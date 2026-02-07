70600db78779a7bc42a368b06dee7670e733fdb3
V <121571470+MrV1777@users.noreply.github.com>
2026-02-07 22:37:41 +0700

    002

--- project_shop/routes/web.php
+++ project_shop/routes/web.php
@@ -10,6 +10,6 @@ Route::post('/register', [AuthController::class, 'register']);
 
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get('/home', function () {
-    return view('home');
+Route::get('/', function () {
+    return view('index');
 })->middleware('auth');
